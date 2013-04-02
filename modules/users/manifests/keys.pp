749a20b85e227191cf35390dbd3c52de153aded0
jdotpz <john.p.schneider@gmail.com>
2013-04-02 16:40:10 -0500

    Fix source bucket for base deployment creds in devops auth key injection

--- modules/users/manifests/keys.pp
+++ modules/users/manifests/keys.pp
@@ -1,7 +1,7 @@
 class users::keys {
 
   exec { 'inject_devops_authorized_keys':
-    command     => "/bin/cat /root/creds/authorized_keys.mofo >> /home/ubuntu/.ssh/authorized_keys",
+    command     => "/bin/cat /root/creds/authorized_keys.ef >> /home/ubuntu/.ssh/authorized_keys",
     unless      => "/bin/grep 8IYEi453ZJTGM8iEBWsx5Mix0047Ez8ZElH /home/ubuntu/.ssh/authorized_keys",
     require     => Class['creds'],
   }
