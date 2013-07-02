9e3036efcb33c9fa3ca85857fefa2df88b87a353
JP Schneider <johns@mozillafoundation.org>
2013-07-02 14:28:22 -0500

    Fix dashes in execs

--- modules/rabbitmq/manifests/init.pp
+++ modules/rabbitmq/manifests/init.pp
@@ -2,7 +2,7 @@ class rabbitmq ( $newuser='edgeflip', $newpass='edgeflip', $newvhost='edgeflip' 
 
   package { 'rabbitmq-server':
     ensure => installed,
-    notify => Exec['add-new-user'],
+    notify => Exec['add_new_user'],
   }
 
   service { 'rabbitmq-server':
@@ -10,31 +10,31 @@ class rabbitmq ( $newuser='edgeflip', $newpass='edgeflip', $newvhost='edgeflip' 
     require => Package['rabbitmq-server'],
   }
 
-  exec { 'add-new-user':
+  exec { 'add_new_user':
     command     => '/usr/sbin/rabbitmqctl add_user $newuser $newpass',
     returns     => [ 0, 100 ],
     require     => [ Package['rabbitmq-server'],
                      Service['rabbitmq-server'], ],
     refreshonly => true,
-    notify      => Exec['add-new-vhost'],
+    notify      => Exec['add_new_vhost'],
   }
 
-  exec { 'add-new-vhost':
+  exec { 'add_new_vhost':
     command     => '/usr/sbin/rabbitmqctl add_vhost $newvhost',
     returns     => [ 0, 100 ],
     require     => [ Package['rabbitmq-server'],
                      Service['rabbitmq-server'],
-                     Exec['add-new-user'], ],
+                     Exec['add_new_user'], ],
     refreshonly => true,
-    notify      => Exec['add-new-permissions'],
+    notify      => Exec['add_new_permissions'],
   }
 
-  exec { 'add-new-permissions':
+  exec { 'add_new_permissions':
     command     => '/usr/sbin/rabbitmqctl set_permissions -p edgehost edgeflip \".*\" \".*\" \".*\"',
     returns     => [ 0, 100 ],
     require     => [ Package['rabbitmq-server'],
                      Service['rabbitmq-server'],
-                     Exec['add-new-vhost'], ],
+                     Exec['add_new_vhost'], ],
     refreshonly => true,
   }
 }
