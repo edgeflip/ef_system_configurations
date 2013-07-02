ad50a80fc850b0bf53bfd96d0447d86e44686c32
JP Schneider <johns@mozillafoundation.org>
2013-07-02 15:54:25 -0500

    Move celeryd.conf into my repo

--- modules/apps/manifests/edgeflipcelery.pp
+++ modules/apps/manifests/edgeflipcelery.pp
@@ -113,7 +113,7 @@ class apps::edgeflipcelery ( $env='production' ) {
 
   file { '/etc/default/celeryd':
     ensure  => file,
-    source  => '/var/www/edgeflipcelery/scripts/celery/celeryd.conf',
+    source  => 'puppet:///modules/apps/edgeflipcelery/celeryd.conf',
     require => [ Package['edgeflipcelery'],
                  Package['rabbitmq-server'], ],
     notify  => Service['celeryd'],
