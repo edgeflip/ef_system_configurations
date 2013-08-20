4ca63cc056440a6a8bf2aad41dc5c3fb9ad57b98
JP Schneider <johns@mozillafoundation.org>
2013-08-20 15:08:15 -0500

    welcome back, sanely named appid=group-with-tag,

--- modules/apps/manifests/edgeflip.pp
+++ modules/apps/manifests/edgeflip.pp
@@ -1,18 +1,14 @@
-class apps::edgeflip ( $env='production', $client='demandaction' ) {
+class apps::edgeflip ( $env='production', $nodetype='web' ) {
 
-case $client {
-'demandaction': {
   case $env {
     'production': {
       $edgeflip_env='production'
-      $edgeflip_hostname='demandaction.edgeflip.com'
+      $edgeflip_hostname='www.edgeflip.com'
     } 'staging': {
       $edgeflip_env='staging'
       $edgeflip_hostname='edgeflip.efstaging.com'
     }
   }
-}
-}
 
   apache2::templatevhost { "$edgeflip_hostname":
     content => template('apps/edgeflip/vhost.erb'),
@@ -29,7 +25,8 @@ case $client {
                  Package['libmysqlclient-dev'],
                  Package['virtualenvwrapper'],
                  Package['graphviz'], ],
-    notify  => [ Exec['fix_perms'], Exec['move_configs'], ],
+    notify  => [ Exec['fix_perms'],
+                 Exec['move_configs'], ],
   }
 
   package { 'build-essential':
@@ -72,7 +69,6 @@ case $client {
 
   file { '/var/www/edgeflip/newrelic.ini':
     ensure  => file,
-    mode    => "0755",
     source  => '/root/creds/app/newrelic.ini',
     owner   => 'www-data',
     group   => 'www-data',
@@ -85,6 +81,57 @@ case $client {
     refreshonly => true,
     require     => Package['edgeflip'],
     notify      => [ Service['apache2'], Exec['fix_perms'], ]
+  }
+
+  if $nodetype == 'celery' {
+
+    # Celery related items
+    group { 'celery':
+      ensure  => present,
+      system  => true,
+    }
+
+    user { 'celery':
+      ensure   => present,
+      system   => true,
+      gid      => 'celery',
+      require  => Group['celery'],
+    }
+
+    file { '/var/run/celery':
+      ensure  => directory,
+      owner   => 'celery',
+      group   => 'celery',
+      require => User['celery'],
+    }
+
+    file { '/var/log/celery':
+      ensure  => directory,
+      owner   => 'celery',
+      group   => 'celery',
+      require => User['celery'],
+    }
+
+    file { '/etc/default/celeryd':
+      ensure  => file,
+      source  => 'puppet:///modules/apps/edgeflip/celeryd.conf',
+      require => Package['edgeflip'],
+      notify  => Service['celeryd'],
+    }
+
+    file { "/etc/init.d/celeryd":
+      ensure  => link,
+      target  => "/var/www/edgeflip/scripts/celery/celeryd",
+      require => Package['edgeflip'],
+      notify  => Service['celeryd'],
+    }
+
+    service { 'celeryd':
+      ensure     => running,
+      hasstatus  => true,
+      hasrestart => true,
+      status     => "/etc/init.d/celeryd status",
+    }
   }
 
   exec { 'fix_perms':
