6223dcfc757191cd80232b728e1522d740ccd897
Mark Rogers <f4nt@f4ntasmic.com>
2014-05-12 10:29:54 -0500

    attempt to fix s3cmd failures

--- modules/creds/manifests/app.pp
+++ modules/creds/manifests/app.pp
@@ -4,20 +4,22 @@ class creds::app ( $env='staging', $app='geppetto',
   $s3cfg = '/root/.s3cfg-creds'
   $source = "$bucket/$env/$app/"
 
-  file { "/root/creds/app":
-    ensure => directory,
-    owner  => root,
-    group  => root,
-    mode   => 0700,
-    notify => Exec['get_app_creds'],
-    require => [ File['/root/creds'],
-                 Exec['install_s3cmd'],
-                 File['/root/.s3cfg-creds'] ],
+  file { '/root/creds/app':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0700',
+    notify  => Exec['get_app_creds'],
+    require => [
+        File['/root/creds'],
+        Exec['install_s3cmd'],
+        File['/root/.s3cfg-creds']
+    ],
   }
 
   exec { 'get_app_creds':
     command     => "/usr/bin/s3cmd get --recursive --force -c ${s3cfg} ${source} /root/creds/app",
-    require     => [ Exec['install_s3cmd'], File['/root/creds/app'], ],
+    require     => [ Exec['install_s3cmd'], File['/root/creds/app'], File['/root/.s3cfg-creds'], ],
     notify      => Exec['inject_authorized_sshkeys'],
   }
 
