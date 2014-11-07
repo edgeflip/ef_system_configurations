e7af3c00b423f533ca6fa44b3cae3008d6315062
Tristan Crockett <tristan@edgeflip.com>
2014-11-07 15:12:11 -0600

    FBSync dynamo writing config

--- modules/apps/manifests/celeryflip.pp
+++ modules/apps/manifests/celeryflip.pp
@@ -25,6 +25,10 @@ class apps::celeryflip ( $env='production', $celerytype='mixed' ) {
           # Tasks that need to be completed for the user to get a response
           $queues='px3,px4,celery'
           $concurrency='2'
+      } 'fbsync_dynamo_writes': {
+          # Feed crawler
+          $queues='user_feeds,bg_upsert,bg_update_edges,bg_bulk_create,bg_partial_save'
+          $concurrency='2'
       } 'fbsync_feed': {
           # Feed crawler
           $queues='user_feeds'
